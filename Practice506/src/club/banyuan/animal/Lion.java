de0c24123241dded5c9885a8e5dfb77b86bc6e2a
Lubingjie <lbjks@qq.com>
2020-05-06 20:50:52 +0800

    506am

--- Practice506/src/club/banyuan/animal/Lion.java
+++
@@ -1,9 +0,0 @@
-package club.banyuan.animal;
-
-public class Lion extends Animal {
-
-  // @Override
-  // public void frozen() {
-  //   System.out.println("lion frozen");
-  // }
-}
--- Practice506/src/club/banyuan/animal/Tiger.java
+++
@@ -1,9 +0,0 @@
-package club.banyuan.animal;
-
-public class Tiger extends Animal {
-
-  @Override
-  public void frozen() {
-    System.out.println("Tiger frozen");
-  }
-}
