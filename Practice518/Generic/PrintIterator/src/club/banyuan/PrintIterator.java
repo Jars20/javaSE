a2027de384b640eaac9aaccc3fc34a0fcecd3584
Lubingjie <lbjks@qq.com>
2020-05-19 15:02:48 +0800

    518 modified and 519am

--- Practice518/Generic/PrintIterator/src/club/banyuan/PrintIterator.java
+++ Practice518/Generic/PrintIterator/src/club/banyuan/PrintIterator.java
@@ -1,9 +1,14 @@
 package club.banyuan;
 
-public class PrintIterator<T> {
-    public static <T> void PrintIter(Iterable<T> input){
-        for (T one : input) {
-            System.out.println(","+one);
+import java.util.Iterator;
+
+public class PrintIterator {                                        //泛型只能用来修饰非静态变量。如果为泛型方法则没这个限制
+    public static <T> void PrintIter(Iterable<T> input) {
+        Iterator<T> iterator = input.iterator();
+        while (iterator.hasNext()) {
+            T temp = iterator.next();
+            System.out.println(temp.toString());
+            System.out.println(",");
         }
     }
 }
