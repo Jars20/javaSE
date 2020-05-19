2b6668f690898962a293478be415044753f476c4
Lubingjie <lbjks@qq.com>
2020-05-19 14:46:24 +0800

    518 modified and 519am

--- Practice518/Generic/PrintIterator/src/club/banyuan/PrintIterator.java
+++ Practice518/Generic/PrintIterator/src/club/banyuan/PrintIterator.java
@@ -1,6 +1,6 @@
 package club.banyuan;
 
-public class PrintIterator {
+public class PrintIterator<T> {
     public static <T> void PrintIter(Iterable<T> input){
         for (T one : input) {
             System.out.println(","+one);
