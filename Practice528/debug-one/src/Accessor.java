b1d6392ada12f0a3be56e9ac059caf94a2fc600d
Lubingjie <lbjks@qq.com>
2020-05-28 20:31:38 +0800

    528 Thread

---
+++ Practice528/debug-one/src/Accessor.java
@@ -0,0 +1,19 @@
+public class Accessor extends Thread {
+
+  private Counter counter;
+
+  public Accessor(String name, Counter theCounter) {
+    super(name);
+    counter = theCounter;
+  }
+
+  /**
+   * 将counter每次加1，加10次，时不时地休息一下。
+   */
+  public void run() {
+    synchronized (counter) {
+      counter.count();
+    }
+  }
+
+}
